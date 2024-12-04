a1504806790bca4b0d65d73a88d020bed23f336d
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-12-04 18:38:13 +0300

    +

--- SQL/DML/SQLQuery6 - AGGREGATION.sql
+++ SQL/DML/SQLQuery6 - AGGREGATION.sql
@@ -1,13 +1,13 @@
-USE AcademySQL;
+USE PD_318_DML;
 
 SELECT 
 		--COUNT(direction_name) 
 		--group_name			AS N'Группа',
 		direction_name		AS N'Направление обучения',
-		COUNT(DISTINCT group_id)		AS N'Количество групп',
-		COUNT(student_id)	AS N'Количество студентов'
-FROM        Groups,Directions,Students
+		COUNT(group_id)		AS N'Количество групп'
+		--COUNT(student_id)	AS N'Количество студентов'
+FROM        Groups,Directions
 WHERE		direction = direction_id
-AND			[group]=group_id
 GROUP BY	direction_name
+HAVING		COUNT(group_id) < 3
 ;
